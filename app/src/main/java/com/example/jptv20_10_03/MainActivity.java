dbf21473d7190b257220e26847753192270a4a96
Exorcist <90632640+HeIIBlazer@users.noreply.github.com>
2023-03-16 11:12:41 +0200

    Save

--- app/src/main/java/com/example/jptv20_10_03/MainActivity.java
+++ app/src/main/java/com/example/jptv20_10_03/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void Sendmessage(View view){
+        TextView textview = findViewById(R.id.textView);
+        EditView editText = findViewById(R.id.editText);
+        textView.setText("Tere Tulemas, "+ editText.getText())
+    }
 }
